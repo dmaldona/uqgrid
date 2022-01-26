b3416b4a9880ed0a4d958e6816bc8521a07036c2
Adrian Maldonado <dmaldona@hawk.iit.edu>
2022-01-26 17:47:43 -0600

    classical model works

--- bin/classical_model.py
+++ bin/classical_model.py
@@ -5,27 +5,41 @@ sys.path.append("..")
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
 from uqgrid.psysdef import Psystem
 from uqgrid.parse import load_psse, add_dyr
 from uqgrid.pflow import runpf
 
-def compute_pmec(pmec, vmag, vang, yred):
-    print(yred)
-    for i in range(len(pmec)):
-        for j in range(len(pmec)):
+def compute_pelec(pelec, vmag, vang, yred):
+    nbus = pelec.size
+    pelec.fill(0.0)
+    for i in range(nbus):
+        for j in range(nbus):
             if i == j:
-                pmec[i] += vmag[i]*vmag[i]*np.real(yred[i, i])
+                pelec[i] += vmag[i]*vmag[i]*np.real(yred[i, i])
             else:
-                pmec[i] += vmag[i]*vmag[j]*(np.imag(yred[i, j])*np.sin(vang[i] - vang[j]) +
+                pelec[i] += vmag[i]*vmag[j]*(np.imag(yred[i, j])*np.sin(vang[i] - vang[j]) +
                         np.real(yred[i, j])*np.cos(vang[i] - vang[j]))
 
+def classic_resfun(t, x, v, yred, pmec, H, D):
+    ngen = v.size
+    F = np.zeros(2*ngen)
+    pelec = np.zeros(ngen)
+    w = x[:ngen]
+    delta = x[ngen:]
+    compute_pelec(pelec, v, delta, yred)
+    for i in range(ngen):
+        F[i] = (1.0/2.0*H[i])*(pmec[i] - pelec[i] - D[i]*w[i])
+        F[ngen + i] = w[i] - 1.0
+    return F
+
 # load static file
-#psys = load_psse(raw_filename="../data/ieee9_v33.raw")
-psys = load_psse(raw_filename="../data/2bus_33.raw")
+psys = load_psse(raw_filename="../data/ieee9_v33.raw")
+#psys = load_psse(raw_filename="../data/2bus_33.raw")
 
 # add dynamics
-#add_dyr(psys, "../data/ieee9bus.dyr")
-add_dyr(psys, "../data/GENROU.dyr")
+add_dyr(psys, "../data/ieee9bus.dyr")
+#add_dyr(psys, "../data/GENROU.dyr")
 
 # Run power flow
 psys.createYbusComplex()
@@ -34,10 +48,11 @@ s_load = psys.get_loadvec()
 
 # Create some data structures.
 ngen = psys.ngens
+gen_inertia = np.zeros(ngen)
+gen_damping = np.zeros(ngen)
 
 # Retrieve admittance matrix
 ymat = np.copy(psys.ybus)
-
 
 # We assume loads are constant admittance.
 for load in psys.loads:
@@ -77,7 +92,11 @@ for i, gen in enumerate(psys.gendyn):
     ybus_aug[ngen + gen.bus, i] -= yint
     ybus_aug[ngen + gen.bus, ngen + gen.bus] += yint
 
+    # inertia and damping
+    gen_inertia[i] = gen.H
+    gen_damping[i] = gen.D
 
+gen_damping[:] = 0.01*gen_inertia[:]
 
 # Compute reduced admittance matrix
 # Actually i can refactor this and not compute ybus_aug
@@ -86,12 +105,37 @@ ynr = ybus_aug[:ngen, ngen:]
 yrn = ybus_aug[ngen:, :ngen]
 yrr = ybus_aug[ngen:, ngen:]
 
+# Kron reduction
 yred = (ynn - np.dot(ynr, np.dot(np.linalg.inv(yrr), yrn)))
 
+## Determine initial state
+w = np.ones(ngen) # ws = 1
+delta = vang
+
 ## Compute mechanical power vector
+## If we have damping, we would do:
+##      pmec = pelec + D*wi
 pmec = np.zeros(ngen)
-compute_pmec(pmec, vmag, vang, yred)
+compute_pelec(pmec, vmag, vang, yred)
+for i in range(ngen):
+    pmec[i] += gen_damping[i]*w[i]
 
-caca = np.imag(yred)
+x0 = np.hstack((w, delta))
+F = classic_resfun(None, x0, vmag, yred, pmec, gen_inertia, gen_damping)
+print(F)
 
-print(pmec)
+x0[0] += 0.1
+
+
+
+## ODE Integrator
+step_size = 1.0/60.0
+
+sol = solve_ivp(classic_resfun, (0.0, 2.0), x0, dense_output=True,
+        args=(vmag, yred, pmec, gen_inertia, gen_damping,),
+        max_step=step_size)
+
+for i in range(ngen):
+    plt.plot(sol.t, sol.y[i, :])
+
+plt.show()
