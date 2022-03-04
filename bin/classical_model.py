1cb50b4b7008f07201d39556d3fc63dfcf82a2aa
Adrian Maldonado <dmaldona@hawk.iit.edu>
2022-03-04 15:12:11 -0600

    work on classical

--- bin/classical_model.py
+++ bin/classical_model.py
@@ -4,10 +4,42 @@ sys.path.append("..")
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
-from uqgrid.psysdef import Psystem
+from uqgrid.psysdef import Psystem, GenGENROU
 from uqgrid.parse import load_psse, add_dyr
 from uqgrid.pflow import runpf
+from numba import jit, njit
 
+WS = 377.0
+
+def system_perturb(nbus=5):
+
+    psys = Psystem()
+
+    busp = np.zeros(nbus)
+
+    psys.add_bus(1, bus_type = 3)
+    for i in range(nbus-1):
+        psys.add_bus(2 + i, bus_type = 2)
+
+    for i in range(nbus):
+        psys.buses[i].set_vinit(1.00000, (np.pi/180.0)*0.0)
+
+    for i in range(nbus - 1):
+        psys.add_branch(i, i + 1, 0.0000, 1.0)
+
+    for i in range(nbus):
+        psys.add_gen(i, 100.0, 0.0, mbase=100.0)
+        psys.add_load(i, 0, 100.0, 0.0)
+    psys.assemble()
+
+    for i in range(nbus):
+        psys.add_gen_dynamics(psys.gens[i],
+            GenGENROU(0, 1.575, 1.512, 0.0000291, 0.39, 0.1733,
+            0.0787, 0.5, 0.0, 6.1, 1.0, 0.05, 0.15))
+
+    return psys
+
+@njit
 def compute_pelec(pelec, vmag, vang, yred):
     """Compute power injection"""
     nbus = pelec.size
@@ -20,8 +52,9 @@ def compute_pelec(pelec, vmag, vang, yred):
                 pelec[i] += vmag[i]*vmag[j]*(np.imag(yred[i, j])*np.sin(vang[i] - vang[j]) +
                         np.real(yred[i, j])*np.cos(vang[i] - vang[j]))
 
-def classic_resfun(t, x, v, yred, pmec, H, D):
+def classic_resfun(t, x, v, yred, pmec2, H, D):
     """ Classical model described in Anderson and Fouad"""
+    
     ngen = v.size
     F = np.zeros(2*ngen)
     pelec = np.zeros(ngen)
@@ -29,9 +62,35 @@ def classic_resfun(t, x, v, yred, pmec, H, D):
     delta = x[ngen:]
     compute_pelec(pelec, v, delta, yred)
     for i in range(ngen):
-        F[i] = (1.0/2.0*H[i])*(pmec[i] - pelec[i] - D[i]*w[i])
+        F[i] = (1.0/(2.0*H[i]))*(pmec[i] - pelec[i] - D[i]*w[i])
         F[ngen + i] = w[i] - 1.0
     return F
+
+def classic_resfun_lin(t, x, J):
+    return np.dot(J, x)
+
+def classic_jacobian(t, x, v, yred, pmec, H, D):
+    """ Jacobian matrix of the classical model """
+    ngen = v.size
+    J = np.zeros((2*ngen, 2*ngen))
+    vang = x[ngen:]
+    for i in range(ngen):
+        ## df1/dw
+        J[i, i] = -D[i]/(2*H[i])
+        ## df2/dw
+        J[ngen + i, i] = 1
+
+        for j in range(ngen):
+            if i != j:
+                J[i, ngen + j] = -v[i]*v[j]*(np.real(yred[i, j])*np.sin(vang[i] - vang[j]) -
+                        np.imag(yred[i, j])*np.cos(vang[i] - vang[j]))
+                J[i, ngen + j] = (1/(2*H[i]))*J[i, ngen +j]
+                J[i, ngen + i] += v[i]*v[j]*(np.real(yred[i, j])*np.sin(vang[i] - vang[j]) -
+                        np.imag(yred[i, j])*np.cos(vang[i] - vang[j]))
+        J[i, ngen + i] = (1/(2*H[i]))*J[i, ngen + i]
+
+    return J
+
 
 def reduced_system(psys):
     """ Compute reduced system via Kron reduction. In this system, we only have
@@ -101,6 +160,7 @@ def reduced_system(psys):
         # inertia and damping
         gen_inertia[i] = gen.H
         gen_damping[i] = gen.D
+        #gen_damping[i] = 0.1*gen.H
 
     # Compute reduced admittance matrix
     # Actually i can refactor this and not compute ybus_aug
@@ -127,28 +187,62 @@ def reduced_system(psys):
     return x0, vmag, yred, pmec, gen_inertia, gen_damping
 
 if __name__ == "__main__":
-    psys = load_psse(raw_filename="../data/ieee9_v33.raw")
+    #psys = load_psse(raw_filename="../data/ieee9_v33.raw")
     #psys = load_psse(raw_filename="../data/2bus_33.raw")
+    #psys = load_psse(raw_filename="../data/IEEE39_v33.raw")
 
-    add_dyr(psys, "../data/ieee9bus.dyr")
+    #add_dyr(psys, "../data/ieee9bus.dyr")
     #add_dyr(psys, "../data/GENROU.dyr")
+    #add_dyr(psys, "../data/IEEE39.dyr")
     
+    psys = system_perturb(2)
+
     x0, vmag, yred, pmec, genH, genD = reduced_system(psys)
 
     # perturb
-    x0[0] += 0.1
+    x = np.copy(x0)
+    #x[0] -= 0.03
 
     ## ODE Integrator
     step_size = 1.0/60.0
-
-    sol = solve_ivp(classic_resfun, (0.0, 2.0), x0, dense_output=True,
+    tend = 11.5
+    sol = solve_ivp(classic_resfun, (0.0, tend), x, dense_output=True,
             args=(vmag, yred, pmec, genH, genD,),
         max_step=step_size)
 
+    ## Linearized system
+    J = classic_jacobian(0, x0, vmag, yred, pmec, genH, genD)
+    dx = x - x0
+    sol2 = solve_ivp(classic_resfun_lin, (0.0, tend), dx, dense_output=True,
+            args=(J,), max_step=step_size)
+
     # Plots
     ngen = pmec.size
-
+    colors = [plt.cm.tab20(i) for i in range(50)]
+    fig = plt.figure(figsize=(10, 8))
     for i in range(ngen):
-        plt.plot(sol.t, sol.y[i, :])
+        plt.plot(sol.t, sol.y[ngen + i, :], color=colors[i])
+        plt.plot(sol2.t, sol2.y[ngen + i, :] + x0[ngen + i], color=colors[i], linestyle='--')
+    plt.xlabel("Time (sec)")
+    plt.ylabel("Rotor frequency dev. (p.u)")
+    plt.show()
 
+
+    # eigenvalue stuff
+    l, u = np.linalg.eig(J)
+
+    u0 = np.real(u[0])
+    sol = solve_ivp(classic_resfun, (0.0, tend), x0 + u0, dense_output=True,
+            args=(vmag, yred, pmec, genH, genD,),
+        max_step=step_size)
+    sol2 = solve_ivp(classic_resfun_lin, (0.0, tend), u0, dense_output=True,
+            args=(J,), max_step=step_size)
+    
+    fig = plt.figure(figsize=(10, 8))
+    for i in range(ngen):
+        plt.plot(sol.t, sol.y[ngen + i, :], color=colors[i])
+        plt.plot(sol2.t, sol2.y[ngen + i, :] + x0[ngen + i], color=colors[i], linestyle='--')
+    plt.title("Eigenresponse")
+    plt.xlabel("Time (sec)")
+    plt.ylabel("Rotor frequency dev. (p.u)")
     plt.show()
