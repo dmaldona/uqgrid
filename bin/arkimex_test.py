4da85a947392d6ec9a2a8a7e76c4982ec9c19cb0
Adrian Maldonado <dmaldona@hawk.iit.edu>
2024-10-09 16:12:11 -0500

    add arkimex example with nine bus, plots and timing

--- bin/arkimex_test.py
+++ bin/arkimex_test.py
@@ -9,40 +9,45 @@ from uqgrid.psysdef import Psystem
 from uqgrid.dynamics import integrate_system
 from uqgrid.parse import load_psse, add_dyr
 from uqgrid.pflow import runpf
+from time import time
 
 # runtime parameters
-zfault = 0.2 # perturbation fault
+zfault = 0.02 # perturbation fault
 dt = 1.0/(120.0) # integration step in seconds
 
 # load static file
-psys = load_psse(raw_filename="../data/ieee9_v33.raw")
+#psys = load_psse(raw_filename="../data/ieee9_v33.raw")
+psys = load_psse(raw_filename="../data/IEEE39_v33.raw")
 
 # add dynamics
-add_dyr(psys, "../data/ieee9bus.dyr")
+#add_dyr(psys, "../data/ieee9bus.dyr")
+add_dyr(psys, "../data/IEEE39.dyr")
 
 # add fault and create initial data structures
 psys.add_busfault(7, zfault, 1.0)
 psys.createYbusComplex()
 v, Sinj = runpf(psys, verbose=True)
 
-# set up parameters
-pnom = np.array([0.5, 0.5, 0.5])
-psys.set_load_parameters(pnom)
-
-tend = 5.0
-tend = 5.0
+tend = 3.0
 ton = 0.3
 
 # Experiment 1: no faut is applied
-results = integrate_system(psys, verbose=True, comp_sens=False,
+begin = time()
+results = integrate_system(psys, verbose=False, comp_sens=False,
                        petsc=True, dt=dt, tend=tend, ton=ton, arkimex=False)
-results2 = integrate_system(psys, verbose=True, comp_sens=False,
+print("Elapsed time for Theta: ", time()-begin)
+
+begin = time()
+results2 = integrate_system(psys, verbose=False, comp_sens=False,
                           petsc=True, dt=dt, tend=tend, ton=ton, arkimex=True)
+print("Elapsed time for Arkimex: ", time()-begin)
 
 # plot generator speeds
 bus_idx = psys.genspeed_idx_set()
 
-fig, axs = plt.subplots(3, 1)
+ngen = len(bus_idx)
+
+fig, axs = plt.subplots(ngen, 1)
 for i, bus in enumerate(bus_idx):
     axs[i].plot(results["tvec"], results["history"][bus,:], label="Theta")
     axs[i].plot(results2["tvec"], results2["history"][bus,:], label="Arkimex")
