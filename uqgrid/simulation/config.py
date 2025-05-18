d8ed84ff47a5d6f835f3a5b2740ed8f306eeefec
Adrian Maldonado <maldonadod@uchicago.edu>
2025-05-18 15:19:28 -0500

    ACTIVSG2000 flatline

--- uqgrid/simulation/config.py
+++ uqgrid/simulation/config.py
@@ -11,6 +11,7 @@ class IntegrationConfig(BaseModel):
     ton: float = Field(0.25, description="Fault activation time.")
     toff: float = Field(0.4, description="Fault deactivation time.")
     petsc: bool = Field(False, description="Enable PETSc integration.")
+    solve_powerflow_dynamics: bool = Field(True, description="Solve power flow before dynamics.")
 
     @field_validator('tend', 'dt', 'ton', 'toff', mode='after')
     def positive_values(cls, v, info):
